d51d32316e1d6488cf4b041c94b851d62acbe65c
pszczepkowski <pszczepkowski87@gmail.com>
2022-02-08 14:01:44 +0100

    finish version 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AccountComponent} from "./account/account.component";
+import {AuthGuard} from "./guard/auth.guard";
+import {ContactComponent} from "./contact/contact.component";
+import {OfferComponent} from "./offer/offer.component";
+import {GalleryComponent} from "./gallery/gallery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'oferta', component: OfferComponent},
+  // {path: 'konto', component: AccountComponent, canActivate: [AuthGuard]},
+  {path: 'galeria', component: GalleryComponent},
+  {path: 'kontakt', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccountComponent } from './account/account.component';
+import {initializeKeycloak} from "./init/keycloak-init.factory";
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import { ContactComponent } from './contact/contact.component';
+import { OfferComponent } from './offer/offer.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccountComponent,
+    ContactComponent,
+    OfferComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory: initializeKeycloak,
+    //   multi: true,
+    //   deps: [KeycloakService]
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
